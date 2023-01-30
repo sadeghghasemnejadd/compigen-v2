0a46b530dcacbce0da60bd955fe49f2c183acc95
sadegh <ghasemnejad.sadegh@gmail.com>
2023-01-30 22:18:04 +0330

    fix svgs and add logo

--- src/components/layout/navbar/Logo/Logo.tsx
+++ src/components/layout/navbar/Logo/Logo.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import LogoIcon from "./../../../../images/svg/logo.svg";
 const Logo: React.FunctionComponent = () => {
-  return <h1>sadegh</h1>;
+  return (
+    <div className="logo">
+      <div className="logo__image">
+        <LogoIcon />
+      </div>
+      <p className="logo__text">
+        Compi<span className="logo__text--special">gen</span>
+      </p>
+    </div>
+  );
 };
 export default Logo;
