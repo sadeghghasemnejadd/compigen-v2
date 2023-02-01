70db96b931a8b5c0ef0e5bb6d9748dd7f53f24f3
sadegh <ghasemnejad.sadegh@gmail.com>
2023-02-01 10:23:06 +0330

    fix language

--- src/components/layout/navbar/language/Lang.tsx
+++ src/components/layout/navbar/language/Lang.tsx
@@ -2,19 +2,24 @@ import React, { useState } from "react";
 import Icon from "@/components/UI/Icon";
 import { ILang } from "@/models/languageInterFace";
 const Lang = () => {
-  const languages: ILang[] = [
+  const [language, setLanguage] = useState<ILang[]>([
     { id: 0, name: "En", default: true },
     { id: 1, name: "Fa" },
-  ];
-  const [language, setLanguage] = useState<ILang>(languages[0]);
-
-  const changeLanguageHandler = () =>
-    setLanguage(languages.find((lang) => lang !== language)!);
+  ]);
+  const changeLanguageHandler = () => {
+    setLanguage((prev) =>
+      prev.map((p) =>
+        p.default ? { ...p, default: false } : { ...p, default: true }
+      )
+    );
+  };
   return (
     <div className="lang">
-      <p className="lang__text">{language.name}</p>
+      <p className="lang__text">
+        {language.find((lang) => lang.default)!.name}
+      </p>
       <p className="lang__text__another" onClick={changeLanguageHandler}>
-        {languages.find((lang) => lang !== language)!.name}
+        {language.find((lang) => !lang.default)!.name}
       </p>
       <Icon size={10} icon="arrow-down3" className="lang__icon" />
     </div>
