4359fb4ed2b05aea9f3565b2296cbae67aacd6e4
sadegh <ghasemnejad.sadegh@gmail.com>
2023-02-06 19:56:36 +0330

    finish hero section

--- src/components/home/Hero/information/HeroInformation.tsx
+++ src/components/home/Hero/information/HeroInformation.tsx
@@ -8,13 +8,13 @@ import Icon from "@/components/UI/Icon";
 const HeroInformation = () => {
   return (
     <div className="hero-information">
-      <div>
+      <div className="hero-information__details">
         <TertiaryHeading text="Frontend developer" />
         <MainHeading text="Based in Iran," />
         <MainHeading text="I'm developer and UI/UX designer." />
         <SimpleText text="If you’re looking for designer to build your brand and grow your bussiness, let’s shake hands with me." />
       </div>
-      <div>
+      <div className="hero-information__btns">
         <Button>Let's Talk</Button>
         <LinkButton color="secondary">
           <p>Download Portfolio</p>
