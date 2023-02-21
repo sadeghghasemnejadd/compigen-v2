36ccd164257f20c10955ccdeeda9fba08a57a9ab
sadegh <ghasemnejad.sadegh@gmail.com>
2023-02-21 22:39:48 +0330

    fix some problems

--- src/components/home/projects/projects.tsx
+++ src/components/home/projects/projects.tsx
@@ -1,9 +1,14 @@
+import LinkButton from "@/components/UI/LinkButton";
 import React from "react";
 import ProjectsNav from "./nav";
 const Projects = () => {
   return (
     <section className="section__padding projects">
       <ProjectsNav />
+      <div></div>
+      <div className="projects__link">
+        <LinkButton color="secondary">Read More</LinkButton>
+      </div>
     </section>
   );
 };
