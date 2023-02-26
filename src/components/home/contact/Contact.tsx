8b0fd09861ee112dbd98c2566587308fb67c75b2
sadegh <ghasemnejad.sadegh@gmail.com>
2023-02-26 13:04:48 +0330

    add contact header

---
+++ src/components/home/contact/Contact.tsx
@@ -0,0 +1,11 @@
+import MainHeading from "@/components/UI/MainHeading";
+import React from "react";
+const Contact = () => {
+  return (
+    <section className="section__padding contact">
+      <MainHeading text="Let's Talk" />
+    </section>
+  );
+};
+
+export default Contact;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/home/Hero/Hero";
 import LastProject from "@/components/home/last-project/LastProject";
 import Education from "@/components/home/education/Education";
 import Projects from "@/components/home/projects/projects";
+import Contact from "@/components/home/contact/Contact";
 export default function Home() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function Home() {
       <LastProject />
       <Education />
       <Projects />
+      <Contact />
     </>
   );
 }
