574d152d1e5e32e7aeb13f034b883099ea1556ee
sadegh <ghasemnejad.sadegh@gmail.com>
2023-02-26 14:23:07 +0330

    fix some bugs in inputs

--- src/components/home/contact/contact-input/ContactInput.tsx
+++ src/components/home/contact/contact-input/ContactInput.tsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 interface IContactProps {
   text: string;
   type: string;
   placeHolder: string;
 }
 const ContactInput = ({ text, type, placeHolder }: IContactProps) => {
+  const [count, setCount] = useState<number>(0);
+  const [textArea, setTextArea] = useState<string>("");
+  const textChangeHandler = (e: any) => {
+    if (e.target.value.length > 1000) return;
+    setTextArea(e.target.value);
+    setCount(e.target.value.length);
+  };
   return (
     <div className="contact-input">
       <label className="contact-input__label">{text}</label>
       {type === "textarea" ? (
-        <textarea
-          placeholder={placeHolder}
-          className="contact-input__input--text-area"
-        />
+        <div className="contact-input__input__text-area">
+          <textarea
+            placeholder={placeHolder}
+            className="contact-input__input__text-area__input"
+            value={textArea}
+            onChange={textChangeHandler}
+          />
+          <div className="contact-input__input__text-area__count">
+            {count}/1000
+          </div>
+        </div>
       ) : (
         <input
           type={type}
