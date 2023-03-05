0bb320425c443eeb344f9fa9163d397e5b7d1011
sadegh <ghasemnejad.sadegh@gmail.com>
2023-03-05 10:44:12 +0330

    add footer component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/sass/main.scss";
 import type { AppProps } from "next/app";
 import Navbar from "@/components/layout/navbar/Navbar";
+import Footer from "@/components/layout/footer/footer";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -11,6 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <main className="main">
         <Component {...pageProps} />
       </main>
+      <footer className="footer section__padding">
+        <Footer />
+      </footer>
     </>
   );
 }
