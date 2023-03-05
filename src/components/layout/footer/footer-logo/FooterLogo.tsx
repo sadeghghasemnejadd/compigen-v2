487a0e5d236848f2c58eb7d8cb5e785ae1e25e67
sadegh <ghasemnejad.sadegh@gmail.com>
2023-03-05 11:39:38 +0330

    finish top of footer

--- src/components/layout/footer/footer-logo/FooterLogo.tsx
+++ src/components/layout/footer/footer-logo/FooterLogo.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import LogoPic from "@/images/svg/logo.svg";
 const FooterLogo = () => {
   return (
-    <div className="footer-logo">
+    <section className="footer-logo">
       <LogoPic />
-    </div>
+    </section>
   );
 };
 
--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import FooterLogo from "./footer-logo";
 import Subscribe from "./subscribe";
+import Link from "next/link";
 const Footer = () => {
+  const menuData = [
+    { id: 0, name: "About me", to: "/" },
+    { id: 1, name: "Projects", to: "/" },
+    { id: 2, name: "Contact", to: "/" },
+  ];
   return (
     <>
       <FooterLogo />
+      <div className="seprate-line"></div>
       <Subscribe />
+      <div className="seprate-line"></div>
+      <section className="footer-menu">
+        <ul className="footer-menu__list">
+          {menuData.map((menu) => (
+            <li key={menu.id} className="footer-menu__list__item">
+              <Link href={menu.to} className="footer-menu__list__item--link">
+                {menu.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 };
