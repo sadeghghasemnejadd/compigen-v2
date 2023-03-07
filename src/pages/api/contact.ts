570ce3aa4d3b249f6b8bc19059ef017e443db30f
sadegh <ghasemnejad.sadegh@gmail.com>
2023-03-07 19:12:08 +0330

    styled email

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -13,9 +13,19 @@ export default function (req: any, res: any) {
   const mailData = {
     from: "compigen.official@gmail.com",
     to: req.body.email,
-    subject: `compigen website From ${req.body.name}`,
+    subject: `Contact with me message`,
     text: req.body.message,
-    html: `<h1>${req.body.message}</h1>`,
+    html: `<h3>Hello dear ${req.body.name}</h3>
+    <p>thank you for your message</p>
+    <h3>your message: ${req.body.message}</h3>
+    <p>Sincerely</p>
+    <p>Sadegh Ghasemnejad</p>
+    <p>Compigen compony</p>
+    <p>+989912330923</p>
+    <a href="mailto:${process.env.EMAIL_ADDRESS}">${process.env.EMAIL_ADDRESS}</a>
+    <a href="compigen.com">compigen.com</a>
+    <a href="https://www.linkedin.com/in/sadegh-ghasemnejad-a4882622a">LinkedIn</a>
+    `,
   };
   transport.sendMail(mailData, function (err: any, info: any) {
     if (err) console.log(err);
